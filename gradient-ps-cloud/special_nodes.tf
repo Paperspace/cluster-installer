3c4cec83c18e86098add54e06b24a09e6d37f741
Marquis C <95641952+marquiswashere@users.noreply.github.com>
2022-03-16 14:27:04 -0400

    feat(gradient-installer): adding workspace specific node (#280)

--- gradient-ps-cloud/special_nodes.tf
+++ gradient-ps-cloud/special_nodes.tf
@@ -1,35 +1,31 @@
-resource "tls_private_key" "admin_management_key" {
-  algorithm = "RSA"
-}
-
-resource "paperspace_script" "gradient_machine_setup" {
+resource "paperspace_script" "gradient_machine_workspace" {
   count       = var.gradient_admin_vm_enabled ? 1 : 0
-  name        = "Gradient Admin Setup"
-  description = "Gradient Admin Setup Script"
+  name        = "Gradient Workspace Controller Node Setup"
+  description = "Gradient Workspace Controller Node Script"
   script_text = templatefile("${path.module}/templates/setup-script.tpl", {
-    kind                         = "admin_public"
+    kind                         = "worker"
     gpu_enabled                  = false
     rancher_command              = rancher2_cluster.main.cluster_registration_token[0].node_command
     ssh_public_key               = tls_private_key.ssh_key.public_key_openssh
-    admin_management_private_key = tls_private_key.admin_management_key.private_key_pem
+    admin_management_private_key = ""
     admin_management_public_key  = tls_private_key.admin_management_key.public_key_openssh
     registry_mirror              = local.region_to_mirror[var.region]
-    pool_type                    = "admin"
-    pool_name                    = "admin-pool"
+    pool_type                    = "workspace"
+    pool_name                    = "workspace"
   })
   is_enabled = true
   run_once   = true
 }
 
-resource "paperspace_machine" "gradient_admin" {
+resource "paperspace_machine" "gradient_workspace_node" {
   count = var.gradient_admin_vm_enabled ? 1 : 0
   depends_on = [
-    paperspace_script.gradient_machine_setup,
+    paperspace_script.gradient_machine_workspace,
     tls_private_key.ssh_key,
   ]
 
   region           = var.region
-  name             = "${var.name}-${var.admin-machine-name-suffix}"
+  name             = "${var.name}-workspace-manager"
   machine_type     = var.machine_type_admin
   size             = var.machine_storage_admin
   billing_type     = "hourly"
