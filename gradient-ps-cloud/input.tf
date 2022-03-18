23dfb066ec739d2c7aa7cb562f3dbd8cab577153
Marquis C <95641952+marquiswashere@users.noreply.github.com>
2022-03-18 11:00:18 -0400

    chore(ps-cloud): default enable workspace vm (#284)

--- gradient-ps-cloud/input.tf
+++ gradient-ps-cloud/input.tf
@@ -108,7 +108,7 @@ variable "gradient_admin_vm_enabled" {
 
 variable "gradient_workspace_vm_enabled" {
   description = "gradient workspace job box is enabled"
-  default     = false
+  default     = true
 }
 
 variable "machine_storage_service" {
