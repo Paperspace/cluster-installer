9676dd56f5c29746683e551f28eb8187cdf1993c
e-r-i-k-a <31043344+e-r-i-k-a@users.noreply.github.com>
2022-05-09 17:28:57 -0400

    chore(notebooks): update docker cache with fastai (#319)

--- gradient-ps-cloud/main.tf
+++ gradient-ps-cloud/main.tf
@@ -590,7 +590,7 @@ module "gradient_processing" {
     "paperspace/nb-pytorch:22.02-py3",
     "paperspace/nb-tensorflow:22.02-tf2-py3",
     "paperspace/fastai:2.0-fastbook-2022-04-25-rc2",
-    "paperspace/fastai:2.0-fastbook-2022-05-04-rc6",
+    "paperspace/fastai:2.0-fastbook-2022-05-09-rc3",
     "paperspace/nb-rapids:22.02-cuda11.2-runtime-ubuntu20.04-py3.9",
     "paperspace/nb-transformers:4.17.0",
     "paperspace/nb-data-science:python-3.9.10",
