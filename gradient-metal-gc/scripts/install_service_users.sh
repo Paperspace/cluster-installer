5e48abbf80ce9d3f931d83309496055fe492a3f9
Marquis <marquis@paperspace.com>
2022-05-19 20:51:51 -0400

    swap users and groups

--- gradient-metal-gc/scripts/install_service_users.sh
+++ gradient-metal-gc/scripts/install_service_users.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# ToDo, revise this as RKE2 does its own containerd wrangling
+
+FLAG_CREATE_KEYS="${1:-0}"
 
 # Pro Tip: Run as root
 if [ "$EUID" -ne 0 ]
@@ -7,21 +8,37 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-containerd_user=containerd
-containerd_group=containerd
+p_user=upaperspace
+p_group=gpaperspace
 
-if [[ ! $(id "${containerd_user}") ]]; then
-    printf 'Creating %s user\n', ${containerd_user}
-    useradd ${containerd_user}
+if [[ ! $(id "${p_user}") ]]; then
+    printf 'Creating %s user\n', ${p_user}
+    useradd ${p_user}
 fi
 
-if [[ ! $(getent group "${containerd_group}") ]]; then
-    printf 'Creating %s group\n', ${containerd_group}
-    groupadd ${containerd_group}
+if [[ ! $(getent group "${p_group}") ]]; then
+    printf 'Creating %s group\n', ${p_group}
+    groupadd ${p_group}
 fi
 
-containerd_sudoers_file=/etc/sudoers.d/100-containerd
-if [[ ! $(stat ${containerd_sudoers_file}) ]]; then
-  printf 'Creating conatinerd group sudoers file at [%s]\n', ${containerd_sudoers_file}
-  echo "%${containerd_group} ALL=(ALL) NOPASSWD:ALL" > ${containerd_sudoers_file}
+usermod -a -G ${p_group} ${p_user}
+
+p_sudoers_file_sudoers_file=/etc/sudoers.d/100-paperspaced
+if [[ ! $(stat ${p_sudoers_file_sudoers_file}) ]]; then
+  printf 'Creating paperspace group sudoers file at [%s]\n', ${p_sudoers_file_sudoers_file}
+  echo "%${p_group} ALL=(ALL) NOPASSWD:ALL" > ${p_sudoers_file_sudoers_file}
 fi
+
+chsh -s /bin/bash ${p_user}
+
+if [ "${FLAG_CREATE_KEYS}" -eq 1 ] ; then
+  ssh-keygen -t rsa -N "" -f clustercreds.key
+  mkdir -p ~/.ssh
+  chown ${p_user} ~
+  mv ./clustercreds.key ~/.ssh/id_rsa
+  stat ./clustercreds.key.pub
+else
+  printf 'Not creating ssh keys this execution\n'
+fi
+
+printf 'User setup complete\n'
