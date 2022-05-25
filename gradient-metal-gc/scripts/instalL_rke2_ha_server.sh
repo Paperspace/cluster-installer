c713283511a420c29e0642470697302199bf2d24
Marquis <marquis@paperspace.com>
2022-05-25 16:09:33 -0400

    script moving and updates

--- gradient-metal-gc/scripts/instalL_rke2_ha_server.sh
+++ gradient-metal-gc/scripts/instalL_rke2_ha_server.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # HA RKE2 Servers need to join the master pool before it can be considered a "peer"
 
+install_rke2() {
 # Locks RKE2 Release Version
 export INSTALL_RKE2_VERSION=v1.21.12+rke2r2
 SYS_D_SERVICE=rke2-server.service
 CONFIG_PATH=/etc/rancher/rke2/config.yaml
 
 # These variables should be set, after the initial RKE2 master server has been created
-RKE2_CONTROL_PLANE_HOST=foo
-RKE2_CLUSTER_TOKEN=bar
+RKE2_CONTROL_PLANE_HOST=${1}
+RKE2_CLUSTER_TOKEN=${2}
 RKE2_TLS_DOMAIN=("kubernetes.default" "kubernetes.default.svc" "kubernetes.default.svc.cluster.local")
 
 # Pro Tip: Run as root
@@ -26,14 +27,29 @@ systemctl start ${SYS_D_SERVICE}
 
 # ToDo, yaml generators
 cat << EOF > ${CONFIG_PATH}
-server: ${RKE2_CONTROL_PLANE_HOST}
+server: https://${RKE2_CONTROL_PLANE_HOST}:9345
 token: ${RKE2_CLUSTER_TOKEN}
 tls-san:
-  - ${RKE2_TLS_DOMAIN[0]}
-  - ${RKE2_TLS_DOMAIN[1]}
-  - ${RKE2_TLS_DOMAIN[2]}
+  - kubernetes.local.com
+disable:
+  - rke2-ingress-nginx
 EOF
-
+}
 
 # https://docs.rke2.io/install/install_options/server_config/
 # journalctl -u rke2-server -f
+
+MASTER_SERVER=${1}
+if [ -z "${MASTER_SERVER}" ]; then
+  printf "MASTER_SERVER Cannot Be Null"
+  exit 1
+fi
+
+CLUSTER_TOKEN=${2}
+if [ -z "${CLUSTER_TOKEN}" ]; then
+  printf "CLUSTER_TOKEN Cannot Be Null\n"
+  exit 1
+fi
+
+
+install_rke2 "${MASTER_SERVER}" "${CLUSTER_TOKEN}"
--- gradient-metal-gc/scripts/install_bastion_tools.sh
+++ gradient-metal-gc/scripts/install_bastion_tools.sh
@@ -21,4 +21,4 @@ sudo apt-get install -y kubectl
 
 sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 sudo chmod 700 get_helm.sh
-sudo /get_helm.sh
+sudo ./get_helm.sh
