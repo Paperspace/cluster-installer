e5524aeced06ac0813c1bcca34c8a1392783798d
Ben Batha <ben.batha@paperspace.com>
2022-07-29 13:45:11 -0400

    Update input.tf

--- gradient-ps-cloud/input.tf
+++ gradient-ps-cloud/input.tf
@@ -147,7 +147,7 @@ variable "machine_storage_worker_gpu" {
 }
 variable "machine_template_id_gpu" {
   description = "GPU template id"
-  default     = "tlwkbhba"
+  default     = "tmun4o2g"
 }
 
 variable "prometheus_resources" {
