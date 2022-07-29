09bc499f4aea8e0318512fe4717915bd38fc14e1
Ben Batha <ben.batha@paperspace.com>
2022-07-29 09:36:50 -0400

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! feat: add docker registry mirror module and use in graphcore

--- gradient-metal-gc/scripts/install_rke2_worker.sh
+++ gradient-metal-gc/scripts/install_rke2_worker.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 
 # Pro Tip: Run as root
-if [ "$EUID" -ne 0 ]
+if [ "${EUID:-}" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
 install_rke2() {
-
   # Locks RKE2 Release Version
   export INSTALL_RKE2_VERSION=v1.21.12+rke2r2
   CONFIG_PATH=/etc/rancher/rke2/config.yaml
@@ -16,11 +16,16 @@ install_rke2() {
 
   RKE2_CONTROL_PLANE_HOST=${1}
   RKE2_CLUSTER_TOKEN=${2}
+  CLUSTER_DOMAIN=${3}
 
   curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE="agent" sh -
 
   rke2_unit="/usr/local/lib/systemd/system/rke2-agent.service"
-  grep -q '^LimitMEMLOCK' "$rke2_unit" && sed -i 's/^LimitMEMLOCK.*/LimitMEMLOCK=infinity/' "$rke2_unit" || echo 'LimitMEMLOCK=infinity' >> "$rke2_unit"
+  if grep -q '^LimitMEMLOCK' "$rke2_unit"; then
+    sed -i 's/^LimitMEMLOCK.*/LimitMEMLOCK=infinity/' "$rke2_unit"
+  else
+    echo 'LimitMEMLOCK=infinity' >> "$rke2_unit"
+  fi
 
   systemctl enable ${SYS_D_SERVICE}
   systemctl daemon-reload
@@ -31,6 +36,11 @@ install_rke2() {
 cat << EOF > ${CONFIG_PATH}
 server: https://${RKE2_CONTROL_PLANE_HOST}:9345
 token: ${RKE2_CLUSTER_TOKEN}
+mirrors:
+  docker.io:
+    endpoint:
+      - "https://docker-registry-mirror.${CLUSTER_DOMAIN}"
+      - "https://index.docker.io"
 EOF
 
   chmod 700 ${CONFIG_PATH}
@@ -42,18 +52,26 @@ EOF
 # journalctl -u rke2-agent -f
 
 
+usage() {
+  echo "usage: install_rke2_worker.sh <control-plane-host> <cluster-token> <cluster-domain>"
+}
 
-MASTER_SERVER=${1}
-if [ -z "${MASTER_SERVER}" ]; then
-  printf "MASTER_SERVER Cannot Be Null"
+CONTROLPLANE_HOST=${1}
+if [ -z "${CONTROLPLANE_HOST:-}" ]; then
+  usage
   exit 1
 fi
 
 CLUSTER_TOKEN=${2}
-if [ -z "${CLUSTER_TOKEN}" ]; then
-  printf "CLUSTER_TOKEN Cannot Be Null\n"
+if [ -z "${CLUSTER_TOKEN:-}" ]; then
+  usage
   exit 1
 fi
 
+CLUSTER_DOMAIN=${3}
+if [ -z "${CLUSTER_DOMAIN:-}" ]; then
+  usage
+  exit 1
+fi
 
-install_rke2 "${MASTER_SERVER}" "${CLUSTER_TOKEN}"
+install_rke2 "${CONTROLPLANE_HOST}" "${CLUSTER_TOKEN}" "${CLUSTER_DOMAIN}"
