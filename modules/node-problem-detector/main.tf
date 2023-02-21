9c273b34ad9bc55fd9a182e9aa09fd691dd56490
Ben Batha <ben.batha@paperspace.com>
2023-02-21 18:39:40 +0000

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Update run

--- modules/node-problem-detector/main.tf
+++ modules/node-problem-detector/main.tf
@@ -132,7 +132,7 @@ resource "kubernetes_cluster_role_binding" "draino" {
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
-    name      = kubernetes_cluster_role.draino_rbac.metadata[0].name
+    name      = kubernetes_cluster_role.draino.metadata[0].name
   }
 
   subject {
