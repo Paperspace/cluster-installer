c4126836f1071507f948d0397d26967ac2e4d387
Ben Batha <ben.batha@paperspace.com>
2023-05-30 20:39:10 -0400

    Update service.tf

--- gradient-ps-cloud/service.tf
+++ gradient-ps-cloud/service.tf
@@ -47,6 +47,7 @@ resource "null_resource" "gradient_service_check" {
     inline = ["/bin/true"]
     connection {
       bastion_host        = local.enable_gradient_lb > 0 : paperspace_machine.gradient_lb[0].public_ip_address : paperspace_machine.gradient_main[0].public_ip_address
+      
       bastion_user        = "paperspace"
       bastion_private_key = tls_private_key.ssh_key.private_key_pem
 
