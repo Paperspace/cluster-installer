c45d78199ba322857f56bec3b06a4a8e737671ad
Ben Batha <bhbatha@gmail.com>
2023-07-18 12:38:07 -0400

    Revert "fix(ps-cloud): make guest health checks more resilient SIT-372 (#447)"
    This reverts commit 115134d4b6deddf654771b0f9b7e74f481936f9a.

--- gradient-ps-cloud/files/linux-guest-health.sh
+++ gradient-ps-cloud/files/linux-guest-health.sh
@@ -9,22 +9,9 @@ readonly UNKNOWN=2
 readonly GUEST_OUTPUT="$1"
 readonly CHECK="$2"
 
-uptime_seconds=$(uptime | awk -F' ' '{print $3}' | awk -F':' '{print $1*3600 + $2*60}')
-
 if [ ! -f "$GUEST_OUTPUT" ]; then
-  if [ "$uptime_seconds" -gt 300 ]; then
-    echo "Guest health output not found and uptime > 5 minutes"
-    exit $NONOK
-  else
-    echo "Still waiting for initial guest health output"
-    exit $UNKNOWN
-  fi
-fi
-
-# update interval is 60 minutes on the health reporter
-if find "$GUEST_OUTPUT" -mmin -61 | read -r; then
-  echo "Guest health output too old"
-  exit $NONOK
+  echo "No guest health output found"
+  exit $UNKNOWN
 fi
 
 error=$(jq -r "try .errors.\"$CHECK\" | join(\",\")" "$GUEST_OUTPUT")
