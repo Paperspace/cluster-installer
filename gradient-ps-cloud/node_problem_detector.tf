73fd1b69e7ef9f3266bfb74bec88422e71a5c6a6
Ben Batha <ben.batha@paperspace.com>
2023-07-19 10:47:09 -0400

    Update node_problem_detector.tf

--- gradient-ps-cloud/node_problem_detector.tf
+++ gradient-ps-cloud/node_problem_detector.tf
@@ -9,6 +9,7 @@ module "node_problem_detector" {
 
   draino_flags = {
     skip_drain = true
+    node_label_expr = "metadata.labels['node-role.kubernetes.io/worker'] == 'true'"
   }
 
   draino_node_selector = {
